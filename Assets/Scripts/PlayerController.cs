21a13341887d0ecddc708437bfb700bb46b95ddf
Catrena <catrenasgraphics@gmail.com>
2022-04-25 12:27:43 -0700

    LessonA

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    public float gravityModifier;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        // rbPlayer.AddForce(Vector3.up * 1000);
+        Physics.gravity *= gravityModifier; //shorthand for physics.gravity = physics.gravity * gravityModifier; less cose, less chance 4 error
+                                          
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        if (spaceDown)
+        {
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
